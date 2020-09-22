75edcf3bd6a86a13aa8a9d691507ea5c023f3edc
Diego Buriti <bs.diego97@gmail.com>
2020-09-22 10:44:20 -0300

    Asynchronous fetch.

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -8,22 +8,23 @@ const Add = () => {
   const apiKey = process.env.REACT_APP_API_KEY;
   console.log(apiKey);
 
-  function handleChange(event) {
+  async function handleChange(event) {
     event.preventDefault();
     setquery(event.target.value);
+    let response;
+    let json;
 
-    fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1&include_adult=false&query=${event.target.value}`,
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        if (!data.errors) {
-          setResults(data.results);
-        } else {
-          setResults([]);
-        }
-      });
+    try {
+      response = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1&include_adult=false&query=${event.target.value}`,
+      );
+      json = await response.json();
+      if (!response.ok) throw new Error(json.message);
+      setResults(json.results);
+    } catch (err) {
+      console.log(err);
+      setResults([]);
+    }
   }
   return (
     <div className="add-page">
